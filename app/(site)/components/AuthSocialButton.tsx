c7fb9ff418eca73f3046077cc852092e57344391
Chayannn <chayanp188@gmail.com>
2023-08-14 23:56:21 +0530

    added authentication ui

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,17 @@
+import { IconType } from "react-icons";
+
+interface AuthSocialButtonProps {
+  icon: IconType;
+  onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+  icon: Icon,
+  onClick,
+}) => {
+  return <button
+  type='button'
+  ></button>;
+};
+
+export default AuthSocialButton;
